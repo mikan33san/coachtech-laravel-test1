502ba8690a5f07d2893673a833b00876d909c314
mikan <m.mch5711kzkn@gmail.com>
2022-01-18 20:07:57 +0900

    'laravel'

---
+++ web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\TestController;
+
+
+Route::get('/building' , [TestController::class, 'index']);
+Route::get('/building/{room?}' , [TestController::class, 'add']);
